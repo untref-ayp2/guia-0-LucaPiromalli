0352b1d86ea611025bdb4444bf6eaf0924c3b0b5
Luca Piromalli <54710080+LucaPiromalli@users.noreply.github.com>
2023-03-21 16:47:04 -0300

    Update array-02.go

--- arreglos-02/array-02.go
+++ arreglos-02/array-02.go
@@ -3,19 +3,17 @@
 y devuelvan la suma y el producto escalar de los vectores
 */
 
-func SubsecuenciaSumaMaxima(arreglo []int) (int, int, int) {
-	sumaMaxima := 0
-	posInicial, posFinal := -1, -1
-	for i := 0; i < len(arreglo); i++ {
-		sumaLocal := 0
-		for j := i; j < len(arreglo); j++ {
-			sumaLocal += arreglo[j]
-			if sumaLocal > sumaMaxima {
-				sumaMaxima = sumaLocal
-				posInicial = i
-				posFinal = j
-			}
+func ProductoYSumaDeVectores(array, array_1 []int) (int, int) {
+	suma := 0
+	productoEscalar := 0
+
+	for i := 0; i < len(array); i++ {
+		for j := i; j < len(array_1); j++ {
+
+			productoEscalar = productoEscalar + array[i]*array_1[j]
+
+			suma = suma + array[i] + array_1[j]
 		}
 	}
-	return sumaMaxima, posInicial, posFinal
+	return productoEscalar, suma
 }
